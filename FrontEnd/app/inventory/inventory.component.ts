e33984dbe9a837915803c11918071d8cd0167d8f
Dell <ronyrajan89@gmail.com>
2018-09-09 19:49:12 +0530

    Added Angular files

---
+++ FrontEnd/app/inventory/inventory.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inventory',
+  templateUrl: './inventory.component.html',
+  styleUrls: ['./inventory.component.css']
+})
+export class InventoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  openCategory() {
+  }
+
+  openAttributes() {
+  }
+
+  openItems() {
+  }
+
+}
